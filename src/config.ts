62ee3c34ba7235014539d236d2c9c4ff12d3ccd5
= <=>
2024-12-18 01:00:28 +0530

    some

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 //export const BACKEND_URL = "http://localhost:3000";
 export const BACKEND_URL = "https://collabdocs-backend.onrender.com";
-export const BACKEND_WEBSOCKET_URL = "ws://collabdocs.onrender.com";
+export const BACKEND_WEBSOCKET_URL = "wss://collabdocs.onrender.com";
